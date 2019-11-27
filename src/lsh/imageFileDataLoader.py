d9aea6d02077fa4723531f8127b35cea25b6c160
Mark Altaweel <maltaweel@ucl.ac.uk>
2019-11-27 19:50:46 +0000

    commit

--- src/lsh/imageFileDataLoader.py
+++ src/lsh/imageFileDataLoader.py
@@ -7,12 +7,15 @@ Created on Nov 27, 2019
 import os
 import csv
 
-cultureData={}
-periodData={}
-timeData={}
-
-def readFile(pathway):
-    with open(pathway,'rU') as csvfile:
+class ImageFile():
+    
+    def readFile(self):
+        
+        self.cultureData={}
+        self.periodData={}
+        self.timeData={}
+        
+        with open(self.input_dir,'rU') as csvfile:
                 reader = csv.DictReader(csvfile)
  
                 for row in reader:
@@ -21,16 +24,21 @@ def readFile(pathway):
                     period=row['Period']
                     culture=row['Culture']
                     
-                    cultureData[fileN]=culture
-                    periodData=[fileN]=period
-                    timeData[fileN]=timeD
+                    self.cultureData[fileN]=culture
+                    self.periodData=[fileN]=period
+                    self.timeData[fileN]=timeD
                     
 
-def runFile():
+    def runFile(self):
+        
+        pn=os.path.abspath(__file__)
+        pn=pn.split("src")[0]
     
-    pn=os.path.abspath(__file__)
-    pn=pn.split("src")[0]
+        self.input_dir=os.path.join(pn,'image_data','imageLink.csv')
+        
     
-    input_dir=os.path.join(pn,'image_data','imageLink.csv')
+    def checkResults(self,file1,file2):
+        
+        print('results')
     
-    readFile()
+        

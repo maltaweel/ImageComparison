b8ad40a2b4b0bba9225f911de96aad500b3371e1
Mark Altaweel <maltaweel@ucl.ac.uk>
2019-12-17 17:13:44 +0000

    commit

--- src/network/shapefileMaker.py
+++ src/network/shapefileMaker.py
@@ -8,10 +8,16 @@ Created on Dec 14, 2019
 import shapefile
 import geopandas as gpd
 import os
+import csv
+
 
 pn=os.path.abspath(__file__)
 pn=pn.split("src")[0]     
+
 countries={}
+geoValues={}
+country_file={}
+
 
 def makePoint(x,y,number):
     w = shapefile.Writer(shapefile.POINT)
@@ -20,8 +26,9 @@ def makePoint(x,y,number):
    
    
 def makeLine(x1,y1,x2,y2,number):
-    w = shapefile.Writer(shapefile.POLYLINE)
-    w.line(parts=[[[x1,y1],[x2,y2]]])
+    path=os.path.join(pn,'network_output','network.shp')
+    w = shapefile.Writer(path,shapefile.POLYLINE)
+    w.line([[[x1,y1],[x2,y2]]])
     w.record(number,'Line')
     
 
@@ -45,13 +52,83 @@ def readInputShape():
     
     
 def runData():
+    pway=os.path.join(pn,'image_data','imageLink.csv')
     
-    print('points finished')
+    try:
+        with open(pway, 'rU') as csvf:
+            reader = csv.DictReader(csvf)
 
-def run():
-    readInputShape()
+            for row in reader:
+                
+                fileName=row['File']
+                mCountry=row['Modern Country']
+                
+                fileName=fileName.split(".")[0]
+                country_file[fileName]=mCountry
+                
+    except IOError:
+        print ("Could not read file:", IOError)     
+
+def matchOutput():
+    pway=os.path.join(pn,'output')
+
+    try:
+        
+        for f in os.listdir(pway):
+            with open(os.path.join(pway,f), 'rU',encoding='utf8',errors='replace') as csvf:
+                reader = csv.DictReader((l.replace('\0', '') for l in csvf))
+            
+                for row in reader:
+                   
+                    f1 = row['File 1'].split(".")[0]
+                    f2 = row['File 2'].split(".")[0]
+
+                    v=row['Similarity']
+                
+                    cntry1=country_file[f1]
+                    cntry2=country_file[f2]
+                
+                    key=cntry1+":"+cntry2
+                
+                    if key in geoValues:
+                        vv=geoValues[key]
+                        geoValues[key]=v+vv
+                    else:
+                        geoValues[key]=v
+                
+
+    except IOError:
+        print ("Could not read file:", IOError) 
+        
+def createOutput():
     
+    for k in geoValues.keys():
+        v=geoValues[k]
+        
+        sp1=k.split(":")[0]
+        sp2=k.split(":")[1]
+        
+        
+        if sp1 in countries:
+            g1=countries[sp1]
+            x1=g1.x
+            y1=g1.y
+        
+        if sp2 in countries:
+            g2=countries[sp2]
+            x2=g2.x
+            y2=g2.y
+        
+        if x1 and y2 is not None:
+            makeLine(x1,y1,x2,y2,v)
+        
+          
+def run():
+   
+    readInputShape()
     runData()
+    matchOutput()
+    createOutput()
     
     print('finished')
 

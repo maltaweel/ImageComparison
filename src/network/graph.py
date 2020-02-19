da92d89e20388d59b2a1ed1fed2dd7d6ae6368a4
Mark Altaweel <maltaweel@ucl.ac.uk>
2020-02-19 00:25:55 +0000

    commit

--- src/network/graph.py
+++ src/network/graph.py
@@ -6,6 +6,8 @@ Created on Nov 8, 2018
 import networkx as nx
 import os
 import pysal
+import geopandas as gpd
+
 
 def createNetwork():
     G=nx.Graph()
@@ -32,27 +34,29 @@ def load():
       
     #The data file path is now created where the data folder and dataFile.csv is referenced
     filename=os.path.join(pn,'network_output','network.shp')
-        
-    shp = pysal.open(filename)
+    
+    poly = gpd.read_file(filename)
+    geometry = poly['geometry']
+    values=poly['value']
+    
+    
     node1=0
     node2=0
     i = 0
     
-    for s in shp:
+    for p in range(0,len(geometry)):
       
-        for p in s._vertices:
+        string1=geometry[p]
+        value=values[i]
+        b=string1.bounds
+        node1=(b[0],b[1])
+        node2=(b[2],b[3])
+        line=(node1,node2,value)
+       # weight=string1['value']
            
-            node1=p[0]
-            weight=p['value']
-           
-            #print(node1)
             
-            link=(node1,node2,weight)
-            
-            
-            
-            links.append(link)
-            i+=1
+        links.append(line)
+        i+=1
                 
   
            

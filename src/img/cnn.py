c99ac025a57c4bf8a100e6088dabdb8f4126cbd6
Mark Altaweel <maltaweel@ucl.ac.uk>
2020-05-27 09:12:56 +0100

    commit

--- src/img/cnn.py
+++ src/img/cnn.py
@@ -10,12 +10,13 @@ import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, Flatten
-import ray
+import tensorflow as tf
 
-#start ray
-ray.init()
 
-@ray.remote    
+
+
+
+   
 def preVariables():
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -24,7 +25,7 @@ def preVariables():
     
     return X_train,y_train,X_test,y_test        
            
-@ray.remote         
+        
 def data_preprocessing (X_train,X_test):
     #reshape data to fit model
     X_train = X_train.reshape(60000,28,28,1)
@@ -32,7 +33,7 @@ def data_preprocessing (X_train,X_test):
     
     return X_train,X_test
 
-@ray.remote       
+      
 def one_hot_encode(y_train,y_test):
     #one-hot encode target column
     y_train = to_categorical(y_train)
@@ -41,28 +42,32 @@ def one_hot_encode(y_train,y_test):
     
     return y_train,y_test
 
-@ray.remote   
+ 
 def build_model():
     #create model
-    model = Sequential()
     
-    #add model layers
-    model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
-    model.add(Conv2D(32, kernel_size=3, activation='relu'))
-    model.add(Flatten())
-    model.add(Dense(10, activation='softmax'))
+    strategy = tf.distribute.MirroredStrategy()
     
-    #compile model using accuracy to measure model performance
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    with strategy.scope():
+        model = Sequential()
+    
+        #add model layers
+        model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(28,28,1)))
+        model.add(Conv2D(32, kernel_size=3, activation='relu'))
+        model.add(Flatten())
+        model.add(Dense(10, activation='softmax'))
+    
+        #compile model using accuracy to measure model performance
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
 
-@ray.remote      
+    
 def train_model(X_train,y_train,X_test,y_test,model):
     #train the model
     model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
 
-@ray.remote    
+   
 def model_predict(model,X_test,y_test):
     #predict first 4 images in the test set
     model.predict(X_test[:4])
@@ -72,14 +77,14 @@ def model_predict(model,X_test,y_test):
 
 def run():
     
-    X_train,y_train,X_test,y_test=ray.get(preVariables.remote())
+    X_train,y_train,X_test,y_test=preVariables()
     plt.imshow(X_train[0])
     
-    X_train,X_test=ray.get(data_preprocessing.remote(X_train,X_test))
-    y_train,y_test=ray.get(one_hot_encode.remote(y_train,y_test))
-    model=build_model.remote()
-    ray.get(train_model.remote(X_train,y_train,X_test,y_test,model))
-    test=ray.get(model_predict.remote(model,X_test,y_test))
+    X_train,X_test=data_preprocessing(X_train,X_test)
+    y_train,y_test=one_hot_encode(y_train,y_test)
+    model=build_model()
+    train_model(X_train,y_train,X_test,y_test,model)
+    test=model_predict(model,X_test,y_test)
     
     print(test[:4])
     plt.imshow(test[:4])
